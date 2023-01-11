43611226e24df8804dd42da72670820384d685f7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-11 11:22:11 +0900

    feat: globalStyle 파일 생성

---
+++ src/assets/styles/globalStyle.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle` {
+  *{  
+    margin: 0;
+    padding: 0;
+  }
+  h1 {
+    font-size: 16px;
+  }
+  ul,
+  li {
+    list-style: none;
+  }
+  textarea,
+  keygen,
+  select,
+  button {
+    all: unset;
+    font-family: inherit;
+    font-size: inherit;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
