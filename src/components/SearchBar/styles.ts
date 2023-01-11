daced186e3c2049133e4cf87f6b320f670edee1d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-11 21:02:56 +0900

    style: SearchBar 스타일링

---
+++ src/components/SearchBar/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const InputBox = styled.div`
+  display: flex;
+  background-color: #ffffff;
+  padding: 30px;
+  width: 35%;
+  min-width: 320px;
+  border-radius: 42px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+`;
