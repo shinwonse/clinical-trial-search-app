ad845961217cc65270651fee02a999ce36622191
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-11 21:03:04 +0900

    style: SearchResultBox 스타일링

---
+++ src/components/SearchResultBox/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  width: 35%;
+  min-width: 320px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 20px;
+`;
+
+export const Title = styled.p``;
