6dd733898765dc14d07c09d32e83a3e835438c12
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-11 23:22:09 +0900

    feat: type 지정 및 검색어 없음 표현

--- src/components/SearchResultBox/index.tsx
+++ src/components/SearchResultBox/index.tsx
@@ -2,17 +2,26 @@ import SearchItem from '../SearchItem';
 
 import { Title, Wrapper } from './styles';
 
+type SearchResultType = {
+  sickCd: string;
+  sickNm: string;
+};
+
 type SearchResultBoxProps = {
-  searchResult: any[];
+  searchResult: SearchResultType[];
 };
 
 function SearchResultBox({ searchResult }: SearchResultBoxProps) {
   return (
     <Wrapper>
       <Title>추천 검색어</Title>
-      {searchResult.map((item) => (
-        <SearchItem key={item?.sickCd} item={item} />
-      ))}
+      {searchResult?.length === 0 ? (
+        <div>검색어 없음</div>
+      ) : (
+        searchResult?.map((item) => (
+          <SearchItem key={item?.sickCd} item={item} />
+        ))
+      )}
     </Wrapper>
   );
 }
