7c9d6bc31561ab5bfeef1cec4f3e42e28ff4ae50
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-12 00:54:10 +0900

    feat: type 추가 및 이벤트 등록

--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -1,10 +1,34 @@
-type SearchItemProps = {
-  item: any;
+import { Dispatch, SetStateAction } from 'react';
+
+type ItemType = {
+  sickCd: string;
+  sickNm: string;
 };
 
-function SearchItem({ item }: SearchItemProps) {
+type SearchItemProps = {
+  item: ItemType;
+  active: boolean;
+  setSelected: Dispatch<SetStateAction<ItemType>>;
+  setHovered: Dispatch<SetStateAction<ItemType>>;
+};
+
+function SearchItem({
+  item,
+  active,
+  setSelected,
+  setHovered,
+}: SearchItemProps) {
   const { sickNm } = item;
-  return <div>{sickNm}</div>;
+  return (
+    <div
+      style={active ? { backgroundColor: 'red' } : {}}
+      onClick={() => setSelected(item)}
+      onMouseEnter={() => setHovered(item)}
+      onMouseLeave={() => setHovered({ sickCd: '', sickNm: '' })}
+    >
+      {sickNm}
+    </div>
+  );
 }
 
 export default SearchItem;
